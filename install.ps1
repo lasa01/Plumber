4706af96b8345997a211fc483722abbea3f6a6dd
Lassi Säike <lassisaike11@gmail.com>
2020-05-03 17:26:41 +0300

    Fix installation scripts: create missing "bin" dir

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 pip install -t io_import_vmf/deps -r requirements.txt --upgrade
+New-Item -Force -ItemType Directory -Path io_import_vmf/bin | Out-Null
 Invoke-WebRequest -outf io_import_vmf/bin/CrowbarCommandLineDecomp.exe "https://github.com/UltraTechX/Crowbar-Command-Line/releases/latest/download/CrowbarCommandLineDecomp.exe"
 Invoke-WebRequest -outf io_import_vmf/bin/CrowbarCommandLineDecomp-License "https://raw.githubusercontent.com/UltraTechX/Crowbar-Command-Line/master/License"
 python pack.py
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 pip install -t io_import_vmf/deps -r requirements.txt --upgrade
+mkdir -p io_import_vmf/bin
 wget -O io_import_vmf/bin/CrowbarCommandLineDecomp.exe "https://github.com/UltraTechX/Crowbar-Command-Line/releases/latest/download/CrowbarCommandLineDecomp.exe"
 wget -O io_import_vmf/bin/CrowbarCommandLineDecomp-License "https://raw.githubusercontent.com/UltraTechX/Crowbar-Command-Line/master/License"
 python pack.py
