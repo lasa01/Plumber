110b869d1e0daeb3b3c3a0177a0be9497fd30ddb
hisanimations <41131633+hisprofile@users.noreply.github.com>
2024-06-19 23:35:45 +0200

    Update unknown_entity.py

--- plumber/asset/unknown_entity.py
+++ plumber/asset/unknown_entity.py
@@ -11,5 +11,6 @@ def import_unknown_entity(entity: UnknownEntity, collection: Collection) -> None
     obj.location = entity.position()
     obj.rotation_euler = entity.rotation()
     obj.scale = entity.scale()
+    obj['props'] = entity.properties()
 
     collection.objects.link(obj)
